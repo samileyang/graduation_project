93ab6bfc0084f1994eb4004e4c2778119eee721a
Yihui Yang <35894512+samileyang@users.noreply.github.com>
2019-01-31 15:28:39 +0800

    Add files via upload

--- bishe/bishe/settings.py
+++ bishe/bishe/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'bishe.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'HOST': 'localhost',
+        'PORT': 3306,
+        'USER': 'root',
+        'PASSWORD': 'xxx',
+        'NAME': 'xxx',
     }
 }
 
