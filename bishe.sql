4e91510afb8d9cb34faf2841933968644d40dc8e
Yihui Yang <35894512+samileyang@users.noreply.github.com>
2019-03-08 22:22:04 +0800

    Add files via upload

--- bishe.sql
+++ bishe.sql
@@ -11,11 +11,72 @@
  Target Server Version : 50720
  File Encoding         : 65001
 
- Date: 07/03/2019 22:20:26
+ Date: 08/03/2019 22:21:27
 */
 
 SET NAMES utf8mb4;
 SET FOREIGN_KEY_CHECKS = 0;
+
+-- ----------------------------
+-- Table structure for add_credit_order
+-- ----------------------------
+DROP TABLE IF EXISTS `add_credit_order`;
+CREATE TABLE `add_credit_order`  (
+  `add_id` int(11) NOT NULL AUTO_INCREMENT,
+  `stu_id` int(11) NULL DEFAULT NULL,
+  `rule_id` int(11) NULL DEFAULT NULL,
+  `score` int(255) NULL DEFAULT NULL,
+  PRIMARY KEY (`add_id`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 1 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_bin ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Table structure for borrow
+-- ----------------------------
+DROP TABLE IF EXISTS `borrow`;
+CREATE TABLE `borrow`  (
+  `book_id` int(11) NOT NULL AUTO_INCREMENT,
+  `book_name` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_bin NULL DEFAULT NULL,
+  `status` binary(255) NULL DEFAULT NULL,
+  PRIMARY KEY (`book_id`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 1 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_bin ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Table structure for borrow_order
+-- ----------------------------
+DROP TABLE IF EXISTS `borrow_order`;
+CREATE TABLE `borrow_order`  (
+  `borrow_id` int(11) NOT NULL AUTO_INCREMENT,
+  `stu_id` int(11) NULL DEFAULT NULL,
+  `book_id` int(11) NULL DEFAULT NULL,
+  `startdate` datetime(0) NULL DEFAULT NULL,
+  `supposedate` datetime(0) NULL DEFAULT NULL,
+  `actdate` datetime(0) NULL DEFAULT NULL,
+  PRIMARY KEY (`borrow_id`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 1 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_bin ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Table structure for borrow_rule
+-- ----------------------------
+DROP TABLE IF EXISTS `borrow_rule`;
+CREATE TABLE `borrow_rule`  (
+  `borrow_rule_id` int(11) NOT NULL AUTO_INCREMENT,
+  `education` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_bin NULL DEFAULT NULL,
+  `day` int(255) NULL DEFAULT NULL,
+  PRIMARY KEY (`borrow_rule_id`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 1 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_bin ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Table structure for certification
+-- ----------------------------
+DROP TABLE IF EXISTS `certification`;
+CREATE TABLE `certification`  (
+  `certi_id` int(11) NOT NULL AUTO_INCREMENT,
+  `course_id` int(11) NULL DEFAULT NULL,
+  `teacher_id` int(11) NULL DEFAULT NULL,
+  `stu_id` int(11) NULL DEFAULT NULL,
+  `status` binary(255) NULL DEFAULT NULL,
+  PRIMARY KEY (`certi_id`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 1 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_bin ROW_FORMAT = Dynamic;
 
 -- ----------------------------
 -- Table structure for classroom
@@ -26,6 +87,17 @@ CREATE TABLE `classroom`  (
   `size` int(11) NULL DEFAULT NULL,
   `status` binary(255) NULL DEFAULT NULL,
   PRIMARY KEY (`classroom_id`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 1 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_bin ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Table structure for credit
+-- ----------------------------
+DROP TABLE IF EXISTS `credit`;
+CREATE TABLE `credit`  (
+  `stu_id` int(11) NOT NULL AUTO_INCREMENT,
+  `year` year NULL DEFAULT NULL,
+  `score` int(255) NULL DEFAULT NULL,
+  PRIMARY KEY (`stu_id`) USING BTREE
 ) ENGINE = InnoDB AUTO_INCREMENT = 1 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_bin ROW_FORMAT = Dynamic;
 
 -- ----------------------------
@@ -56,6 +128,17 @@ CREATE TABLE `dorm`  (
 ) ENGINE = InnoDB AUTO_INCREMENT = 1 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_bin ROW_FORMAT = Dynamic;
 
 -- ----------------------------
+-- Table structure for lost_history
+-- ----------------------------
+DROP TABLE IF EXISTS `lost_history`;
+CREATE TABLE `lost_history`  (
+  `lost_id` int(11) NOT NULL AUTO_INCREMENT,
+  `stu_id` int(11) NULL DEFAULT NULL,
+  `book_id` int(11) NULL DEFAULT NULL,
+  PRIMARY KEY (`lost_id`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 1 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_bin ROW_FORMAT = Dynamic;
+
+-- ----------------------------
 -- Table structure for major
 -- ----------------------------
 DROP TABLE IF EXISTS `major`;
@@ -63,6 +146,83 @@ CREATE TABLE `major`  (
   `major_id` int(11) NOT NULL AUTO_INCREMENT,
   `major_name` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_bin NULL DEFAULT NULL,
   PRIMARY KEY (`major_id`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 1 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_bin ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Table structure for orders
+-- ----------------------------
+DROP TABLE IF EXISTS `orders`;
+CREATE TABLE `orders`  (
+  `order_id` int(11) NOT NULL AUTO_INCREMENT,
+  `product_id` int(11) NULL DEFAULT NULL,
+  `buyer_id` int(11) NULL DEFAULT NULL,
+  `seller_comm` int(255) NULL DEFAULT NULL,
+  `buyer_comm` int(255) NULL DEFAULT NULL,
+  `price` int(10) NULL DEFAULT NULL,
+  PRIMARY KEY (`order_id`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 1 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_bin ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Table structure for penalty
+-- ----------------------------
+DROP TABLE IF EXISTS `penalty`;
+CREATE TABLE `penalty`  (
+  `stu_pen_id` int(255) NOT NULL AUTO_INCREMENT,
+  `stu_id` int(11) NULL DEFAULT NULL,
+  `pen_money` int(255) NULL DEFAULT NULL,
+  `education` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_bin NULL DEFAULT NULL,
+  PRIMARY KEY (`stu_pen_id`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 1 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_bin ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Table structure for products
+-- ----------------------------
+DROP TABLE IF EXISTS `products`;
+CREATE TABLE `products`  (
+  `pro_id` int(11) NOT NULL AUTO_INCREMENT,
+  `pro_name` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_bin NULL DEFAULT NULL,
+  `price_org` int(255) NULL DEFAULT NULL,
+  `price_cur` int(255) NULL DEFAULT NULL,
+  `seller_id` int(11) NULL DEFAULT NULL,
+  `location` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_bin NULL DEFAULT NULL,
+  `status` binary(255) NULL DEFAULT NULL,
+  PRIMARY KEY (`pro_id`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 1 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_bin ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Table structure for roommate_comment
+-- ----------------------------
+DROP TABLE IF EXISTS `roommate_comment`;
+CREATE TABLE `roommate_comment`  (
+  `comm_id` int(11) NOT NULL AUTO_INCREMENT,
+  `dorm_id` int(11) NULL DEFAULT NULL,
+  `comm_maker_id` int(11) NULL DEFAULT NULL,
+  `student_id` int(11) NULL DEFAULT NULL,
+  `score` int(255) NULL DEFAULT NULL,
+  PRIMARY KEY (`comm_id`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 1 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_bin ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Table structure for rules
+-- ----------------------------
+DROP TABLE IF EXISTS `rules`;
+CREATE TABLE `rules`  (
+  `rule_id` int(11) NOT NULL AUTO_INCREMENT,
+  `rule_name` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_bin NULL DEFAULT NULL,
+  `score` int(255) NULL DEFAULT NULL,
+  PRIMARY KEY (`rule_id`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 1 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_bin ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Table structure for scholarship
+-- ----------------------------
+DROP TABLE IF EXISTS `scholarship`;
+CREATE TABLE `scholarship`  (
+  `sch_id` int(255) NOT NULL AUTO_INCREMENT,
+  `stu_id` int(255) NULL DEFAULT NULL,
+  `status` binary(255) NULL DEFAULT NULL,
+  `stu_comm` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_bin NULL DEFAULT NULL,
+  PRIMARY KEY (`sch_id`) USING BTREE
 ) ENGINE = InnoDB AUTO_INCREMENT = 1 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_bin ROW_FORMAT = Dynamic;
 
 -- ----------------------------
