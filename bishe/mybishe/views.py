080589b0326763a046cbaeae3dfc7a101e1af0bb
Yihui Yang <35894512+samileyang@users.noreply.github.com>
2019-03-13 20:50:12 +0800

    Add files via upload

--- bishe/mybishe/views.py
+++ bishe/mybishe/views.py
@@ -14,7 +14,6 @@ def stu_info(request):
 	major_id = request.POST.get('major_id')
 	stu_edu = request.POST.get('stu_edu')
 	instructor_id = request.POST.get('instructor_id')
-	
 	try:
 		dorm = models.Dorm.objects.get(bed_id = bed_id)
 		students = models.Student(stu_pwd = stu_pwd,stu_name=stu_name,stu_year=year,major=major_id,stu_edu=stu_edu,instructor_id=instructor_id,dorm_id=dorm_id,bed_id=bed_id,money = money)
@@ -24,3 +23,12 @@ def stu_info(request):
 	except:
 		pass
 	return render(request,'jwc/stu_info.html',{'dorms': dorms,'majors' : majors,'educations': educations,'instructors': instructors})
+
+def stu_login(request):
+	stu_id = request.POST.get('stu_id')
+	stu_pwd = request.POST.get('stu_pwd')
+	return render(request,'student/stu_login.html')
+
+
+def stu_index(request):
+	return render(request,'student/stu_index.html')
